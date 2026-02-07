c6400950011611e86e3bdf0f9307dee6930ac2ae
Oleksandr Simochko <vhsb721@gmail.com>
2026-02-07 15:58:16 +0100

    Solution

--- app/players/player.py
+++ app/players/player.py
@@ -1,14 +1,14 @@
-from abc import ABC, abstractmethod
+    from abc import ABC, abstractmethod
 
 
-class Player(ABC):
-    def __init__(self, nickname: str) -> None   :
-        self.nickname = nickname
+    class Player(ABC):
+        def __init__(self, nickname: str) -> None:
+            self.nickname = nickname
 
-    @abstractmethod
-    def get_rating(self) -> int:
-        pass
+        @abstractmethod
+        def get_rating(self) -> int:
+            pass
 
-    @abstractmethod
-    def player_info(self) -> str:
-        pass
+        @abstractmethod
+        def player_info(self) -> str:
+            pass
